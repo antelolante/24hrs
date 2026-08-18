8c0587d90bb97daa660847d7a535b9fca923cd07
antelolante <67021364+antelolante@users.noreply.github.com>
2026-08-18 03:45:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ createSpotMarker(
         <h3>🔵 Spot 2</h3>
 
         <img
-            src="pos2.jpg"
+            src="images/pos2.JPG"
             class="spot-photo"
             alt="LDDU Spotting Position 2"
         >
@@ -36,103 +36,6 @@ createSpotMarker(
         <p>
             📸 Great all-around
             spotting location.
-        </p>
-
-    </div>
-`);
-
-
-// ==========================================
-// SPOT 3
-// ==========================================
-
-createSpotMarker(
-    42.5573083097695,
-    18.27213111634963,
-    "3",
-    "#ff9d00"
-)
-.addTo(map)
-.bindPopup(`
-    <div class="popup spot3">
-
-        <h3>🟠 Spot 3</h3>
-
-        <img
-            src="pos3.jpg"
-            class="spot-photo"
-            alt="LDDU Spotting Position 3"
-        >
-
-        <p>
-            🧱 From the wall you can see
-            aircraft on the taxiway and
-            the entire General Aviation apron.
-        </p>
-
-        <p>
-            ✈️ Aircraft vacating via
-            <strong>taxiway E</strong>
-            can be photographed very well.
-        </p>
-
-        <p>
-            📍 42.557308, 18.272131
-        </p>
-
-        <p>
-            📸 Excellent for taxiing
-            and vacating shots.
-        </p>
-
-        <p>
-            ⚠️ Stay outside restricted
-            airport areas.
-        </p>
-
-    </div>
-`);
-
-
-// ==========================================
-// SPOT 4 - HNK KONAVLJANIN
-// ==========================================
-
-createSpotMarker(
-    42.5544013524664,
-    18.282621230913833,
-    "4",
-    "#ff4444"
-)
-.addTo(map)
-.bindPopup(`
-    <div class="popup spot4">
-
-        <h3>🔴 Spot 4 - HNK Konavljanin</h3>
-
-        <img
-            src="pos4.jpg"
-            class="spot-photo"
-            alt="LDDU HNK Konavljanin spotting position"
-        >
-
-        <p>
-            ⚽ HNK Konavljanin
-        </p>
-
-        <p>
-            ✈️ Very good when
-            <strong>RWY 29</strong>
-            is in use.
-        </p>
-
-        <p>
-            📍 42.554401, 18.282621
-        </p>
-
-        <p>
-            📸 Excellent for aircraft
-            approaching runway 29.
         </p>
 
     </div>
